896192860e71a3af15e86b41e41c8f1966957c3a
Richard Smith <mrrichardasmith@gmail.com>
2022-02-26 22:59:07 +0000

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, EqualTo, Email
+from models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class DestinationForm(FlaskForm):
+    studying = StringField('studying')
+    country = StringField('country')
+    description = StringField('description')
+    submit = SubmitField('Post')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class VoteAttendingForm(FlaskForm):
+    vote_attending = BooleanField('Attending Next Meeting')
+    submit = SubmitField('Post')
